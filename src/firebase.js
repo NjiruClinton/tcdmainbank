0cfa5429d652d274f4629a11940fb463491a4124
Clinton Njiru <njiruclinton56@gmail.com>
2024-03-24 19:12:45 +0300

    Path traversal in webpack-dev-middleware

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHb6JQTEMsKhgynJgMG_6wKrKEkXrh_3s",
-    authDomain: "tcdbank-2021f.firebaseapp.com",
-    projectId: "tcdbank-2021f",
-    storageBucket: "tcdbank-2021f.appspot.com",
-    messagingSenderId: "923143263314",
-    appId: "1:923143263314:web:52c9cf690b3b969e85bb8b",
-    measurementId: "G-ZMEHLQMHC6"
+   // configs
   };
 
 // Initialize Firebase and Firebase Authentication
